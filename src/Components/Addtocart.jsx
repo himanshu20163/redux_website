4e2dd4d9018d7c6a30d25276a583d4bd415caca4
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-04-14 15:44:19 +0530

    Update Addtocart.jsx

--- src/Components/Addtocart.jsx
+++ src/Components/Addtocart.jsx
@@ -3,7 +3,7 @@ import "./addcard.css"
 import { useContext } from 'react';
 import { mycontext } from '../App';
 
-const Addtocart = ({carddata, setcarddata}) => {
+const Add_To_Cart = ({carddata, setcarddata}) => {
   const data = useContext(mycontext);
     const removefunc = (id)=>{
        const newversersion = carddata.filter((ele,index)=>{
@@ -33,4 +33,4 @@ const Addtocart = ({carddata, setcarddata}) => {
   )
 }
 
-export default Addtocart
+export default Add_To_Cart
