311d8c6752ccde67ca5c92062dbe4b05a77801a0
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-04-14 15:31:09 +0530

    Update Addtocart.jsx

--- src/Components/Addtocart.jsx
+++ src/Components/Addtocart.jsx
@@ -1,21 +1,38 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
+import React from 'react'
+import "./addcard.css"
+import { useContext } from 'react';
+import { mycontext } from '../App';
 
-const Addtocart = ({ first, second }) => {
-
-  const [cart,setcart] = useState();
-
-  const storedCartItems = localStorage.getItem('cartItems');
-  
-  console.log(storedCartItems);
-  setcart(storedCartItems);
-  
+const Add_To_Cart = ({carddata, setcarddata}) => {
+  const data = useContext(mycontext);
+    const removefunc = (id)=>{
+       const newversersion = carddata.filter((ele,index)=>{
+           return id !== index
+       })
+       setcarddata(newversersion)
+       data.count = data.count-1;
+       data.setcount(data.count);
+    }
   return (
-    <div>
-      <h1>Add to cart</h1>
-      
-    </div>
+    <>
+
+    {carddata.length > 0 ? <div className='mainboxcard'>
+        {
+            carddata.map((ele,id)=>{
+                return <div className='addcards'>
+                    <div className='pehla'><img src={ele.image}></img></div>
+                    <div className='dusra'>
+                    <h2>{ele.title}</h2>
+                    <p>Price:{ele.price}</p>
+                    </div>
+                    <div className='teesra'><button onClick={()=>removefunc(id)}>Remove</button></div>
+                </div>
+            })
+        }
+    </div> : <h1 style={{color:"red" , textAlign:"center"}}>Your Cart is empty</h1>}
+    
+    </>
   )
 }
 
-export default Addtocart
+export default Add_To_Cart
